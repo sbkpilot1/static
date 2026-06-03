7a771ef659b8f0ac816839df0fe33958286a28e3
Kevin Arouza <sbkpilot1@gmail.com>
2026-06-03 07:08:21 +0200

    Update

---
+++ indiafund.js
@@ -0,0 +1,19 @@
+const urls = [
+ "https://www.moneycontrol.com/mutual-funds/nav/axis-large-cap-fund-regular-plan/MAA009",
+ "https://www.moneycontrol.com/mutual-funds/nav/canara-robeco-large-and-mid-cap-fund/MCA033",
+ "https://www.moneycontrol.com/mutual-funds/nav/canara-robeco-large-and-mid-cap-fund/MCA033",
+ "https://www.moneycontrol.com/mutual-funds/nav/kotak-large-midcap-fund/MKM067",
+ "https://www.moneycontrol.com/mutual-funds/nav/mirae-asset-aggressive-hybrid-fund-growth/MMA144",
+ "https://www.moneycontrol.com/mutual-funds/nav/sundaram-large-cap-fund-regular-plan/MSN1536",
+ "https://www.moneycontrol.com/mutual-funds/nav/uti-large-cap-fund-growth/MUT051"
+];
+
+
+const fundMap = new Map();
+
+urls.forEach(u => {
+	const v = getFundValue(u);
+	fundMap.put(
+});
+
+"latestNAV":"255.82"
